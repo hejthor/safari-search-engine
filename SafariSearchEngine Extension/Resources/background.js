7a1619e6bca794bb78fb25329d793029820e6e02
Thor ✨ <hello@thor.computer>
2024-09-15 12:56:49 +0200

    Implemented functionality

--- SafariSearchEngine Extension/Resources/background.js
+++ SafariSearchEngine Extension/Resources/background.js
@@ -1,6 +1,28 @@
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
+chrome.storage.sync.get('customSearchURL', (data) => {
+    const customSearchURL = "https://www.perplexity.ai/search/?q=";
 
-    if (request.greeting === "hello")
-        return Promise.resolve({ farewell: "goodbye" });
+    chrome.webNavigation.onBeforeNavigate.addListener((details) => {
+        const url = new URL(details.url);
+        const query = url.searchParams.get("p") || url.searchParams.get("q");
+
+        if (query) {
+            let newURL = "";
+
+            if (details.url.includes("google.com/search")) {
+                newURL = customSearchURL + encodeURIComponent(query);
+            } else if (details.url.includes("yahoo.com/search")) {
+                newURL = customSearchURL + encodeURIComponent(query);
+            } else if (details.url.includes("bing.com/search")) {
+                newURL = customSearchURL + encodeURIComponent(query);
+            } else if (details.url.includes("duckduckgo.com/")) {
+                newURL = customSearchURL + encodeURIComponent(query);
+            } else if (details.url.includes("ecosia.org/search")) {
+                newURL = customSearchURL + encodeURIComponent(query);
+            }
+
+            if (newURL) {
+                chrome.tabs.update(details.tabId, { url: newURL });
+            }
+        }
+    }, { url: [{ hostContains: 'google.com' }, { hostContains: 'yahoo.com' }, { hostContains: 'bing.com' }, { hostContains: 'duckduckgo.com' }, { hostContains: 'ecosia.org' }] });
 });
