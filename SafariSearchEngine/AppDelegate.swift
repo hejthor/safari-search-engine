c86fd5ed1eab41100bc40b6f1a5473927e467834
Thor ✨ <hello@thor.computer>
2024-09-15 11:54:39 +0200

    Initial commit

---
+++ SafariSearchEngine/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  SafariSearchEngine
+//
+//  Created by Thor Lindberg on 9/15/24.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
---
+++ SafariSearchEngineTests/SafariSearchEngineTests.swift
@@ -0,0 +1,17 @@
+//
+//  SafariSearchEngineTests.swift
+//  SafariSearchEngineTests
+//
+//  Created by Thor Lindberg on 9/15/24.
+//
+
+import Testing
+@testable import SafariSearchEngine
+
+struct SafariSearchEngineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
